a9b88c6874870c2b49128b7cc718771971167701
Stefan Zeller <github@p80.ch>
2018-03-03 12:28:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# mysql-event-emitter
+A mysql / mariadb change emitter.  
+
+_For now it will only tell you \*that\* something changed - not what changed._
+
+It is tested against `mariadb 10.1.31`.
+
+
+## Install
+`npm install mysql-event-emitter`
+
+
+## Setup
+### Mysql Server
+Enable binary log replication in `/etc/mysql/my.cnf`
+```
+[mysqld]
+server-id        = 1
+log_bin          = /var/log/mysql/mysql-bin
+log_bin_index    = /var/log/mysql/mysql-bin.index
+binlog-format    = row
+```
+
+### Mysql User
+Give your user the rights to read binary logs
+```sql
+GRANT REPLICATION SLAVE, REPLICATION CLIENT, SELECT ON *.* TO '[USER]'@'[HOST]'
+```
+
+
+## Usage
+```js
+const MyEmitter = require('mysql-event-emitter');
+
+var mye =  MyEmitter({
+  "mysql": {
+    "user": "[USER]",
+    "password": "[PWD]"
+  }
+});
+
+mye.on('change', function(db, table, event){
+  console.log('An ' + event + ' occured in ' + db + '.' + table);
+}):
+
+mye.on('insert', function(db, table){
+  console.log('An insert occured in ' + db + '.' + table);
+}):
+
+// Vanilla Events
+mye.on('MyDB.MyTable.insert', function(){
+  console.log('An insert occured in MyDB.MyTable');
+}):
+
+mye.on('MyDB.MyTable', function(event){
+  console.log('An ' + event + ' occured in MyDB.MyTable');
+}):
+
+mye.on('MyDB', function(table, event){
+  console.log('An ' + event + ' occured in MyDB.' + table);
+}):
+
+mye.start(function(err){
+  if (err) throw err;
+  console.log('started');
+});
+
+```
+
+
+## Options
+- For a single instance no binlog option is needed
+- Any [mysql](https://www.npmjs.com/package/mysql) [connection](https://www.npmjs.com/package/mysql#connection-options) and [pool](https://www.npmjs.com/package/mysql#pool-options) option is supported.  
+
+**Defaults**
+```js
+{  
+  // mysql pool options
+  "mysql": {
+    "[socket]": "[SOCKET]",
+    "[host]":   "127.0.0.1",
+    "user":     "[USER]",
+    "password": "[PWD]",
+    "[database]": null        // (is not required)
+  }
+  // binlog options
+  "binlog": {
+    "slaveId": 1,             // Needs to be counted up, if more than one instance is running  
+    "recoverTimeout": 240,    // Time in ms between reconnection attempts. (Eg. on a mysql server restart)
+  }
+}
+```
+
+
+## Events
+Event        | Data              | .
+------------ | ----------------- | ---
+connected    | | 
+disconnected | | 
+reconnecting | | 
+recovering   | | 
+|
+error        | Error | 
+|
+change       | DB, Table, Event  | Any of the following events
+insert       | DB, Table | 
+update       | DB, Table | 
+delete       | DB, Table | 
+truncate     | DB, Table | 
+
+### Vanilla Events
+For your individual db's and tables, vanilla events can be defined: `mye.on('MyDB.MyTable.insert')`.
+Event                      | Data
+-------------------------- | ---
+[TABLE]                    | Event
+[TABLE].[EVENT]            | 
+[DATABASE]                 | Table, Event
+[DATABASE].[TABLE]         | Event
+[DATABASE].[TABLE].[EVENT] | 
---
+++ index.js
@@ -0,0 +1,233 @@
+/* jshint node:true */
+/* jshint esversion: 6 */
+/* jshint strict:false */
+"use strict";
+
+
+const MyBinlogEmitter = require('mysql-binlog-emitter');
+const MyBinlogEvent = MyBinlogEmitter.Events;
+const BinlogEvent = MyBinlogEmitter.BinlogEvents;
+
+const EventEmitter = require('events');
+
+
+class MyEmitter extends EventEmitter
+{
+
+  /**
+   * @param Object conf
+   * @param Object conf.mysql                       - mysql pool connection options; 
+   * @param Object [conf.binlog=null]           
+   * @param Object [conf.binlog.slaveId=1]          - When using ultiple clients against the same mysql server this ID must be counted up
+   * @param Object [conf.binlog.recoverTimeout=240] - Time in ms between reconnection attempts
+   */
+  constructor(conf)
+  {
+    super();
+
+
+    this._mbe = new MyBinlogEmitter(conf);
+
+    this._mbe.on(MyBinlogEvent.CONNECTED, this.emit.bind(this, 'connected'));
+    this._mbe.on(MyBinlogEvent.DISCONNECTED, this.emit.bind(this, 'disconnected'));
+    this._mbe.on(MyBinlogEvent.RECONNECTING, this.emit.bind(this, 'reconnecting'));
+    this._mbe.on(MyBinlogEvent.RECOVERING, this.emit.bind(this, 'recovering'));
+
+    this._mbe.on(MyBinlogEvent.ERROR, this.emit.bind(this, 'error'));
+
+
+    this._mbe.on(BinlogEvent.TABLE_MAP_EVENT, this._table_map.bind(this));
+
+    this._mbe.on(BinlogEvent.WRITE_ROWS_EVENTv0, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.WRITE_ROWS_EVENTv1, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.WRITE_ROWS_EVENTv2, this._rows.bind(this));
+
+    this._mbe.on(BinlogEvent.UPDATE_ROWS_EVENTv0, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.UPDATE_ROWS_EVENTv1, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.UPDATE_ROWS_EVENTv2, this._rows.bind(this));
+
+    this._mbe.on(BinlogEvent.DELETE_ROWS_EVENTv0, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.DELETE_ROWS_EVENTv1, this._rows.bind(this));
+    this._mbe.on(BinlogEvent.DELETE_ROWS_EVENTv2, this._rows.bind(this));
+
+    this._mbe.on(BinlogEvent.QUERY_EVENT, this._query.bind(this));
+
+
+    var events = [
+      'connected',
+      'disconnected',
+      'reconnecting',
+      'recovering',
+
+      'error',
+
+      'change',
+      'insert',
+      'update',
+      'delete',
+      'truncate',
+    ];
+    this._hasVanillaEvents = false;
+    this.on('newListener', function(event, listener)
+    {
+      if (events.indexOf(event) == -1)
+      {
+        this._hasVanillaEvents = true;
+      }
+    }.bind(this));
+
+
+    this._tm = null; // table map
+  }
+
+  _table_map(packet)
+  {
+    if (!packet.data)
+    {
+      this.emit('error', new Error('No packet data'), packet);
+      return;
+    }
+    else if (!packet.data.tableId)
+    {
+      this.emit('error', new Error('No packet table id'), packet);
+      return;
+    }
+
+    this._tm = {
+      id: packet.data.tableId,
+      schema: packet.data.schemaName,
+      table: packet.data.tableName,
+    };
+  }
+
+  _rows(packet)
+  {
+    if (!this._tm)
+    {
+      this.emit('error', new Error('No table map'));
+      return;
+    }
+    else if (!packet.data)
+    {
+      this.emit('error', new Error('No packet data'), packet);
+      return;
+    }
+    else if (!packet.data.tableId)
+    {
+      this.emit('error', new Error('No packet table id'), packet);
+      return;
+    }
+    else if (this._tm.id != packet.data.tableId)
+    {
+      this.emit('error', new Error('Wrong table id'));
+      return;
+    }
+
+    switch (packet.eventType)
+    {
+      case BinlogEvent.WRITE_ROWS_EVENTv0:
+      case BinlogEvent.WRITE_ROWS_EVENTv1:
+      case BinlogEvent.WRITE_ROWS_EVENTv2:
+        this.emit('insert', this._tm.schema, this._tm.table);
+        this.emit('change', this._tm.schema, this._tm.table, 'insert');
+        this._vanilla(this._tm.schema, this._tm.table, 'insert');
+        break;
+
+      case BinlogEvent.UPDATE_ROWS_EVENTv0:
+      case BinlogEvent.UPDATE_ROWS_EVENTv1:
+      case BinlogEvent.UPDATE_ROWS_EVENTv2:
+        this.emit('update', this._tm.schema, this._tm.table);
+        this.emit('change', this._tm.schema, this._tm.table, 'update');
+        this._vanilla(this._tm.schema, this._tm.table, 'update');
+        break;
+
+      case BinlogEvent.DELETE_ROWS_EVENTv0:
+      case BinlogEvent.DELETE_ROWS_EVENTv1:
+      case BinlogEvent.DELETE_ROWS_EVENTv2:
+        this.emit('delete', this._tm.schema, this._tm.table);
+        this.emit('change', this._tm.schema, this._tm.table, 'delete');
+        this._vanilla(this._tm.schema, this._tm.table, 'delete');
+        break;
+
+      default:
+        this.emit('error', new Error('Event type not found'));
+    }
+  }
+
+  _query(packet)
+  {
+    if (!packet.data)
+    {
+      this.emit('error', new Error('No packet data'), packet);
+      return;
+    }
+    else if (!packet.data.query)
+    {
+      this.emit('error', new Error('No query'), packet);
+      return;
+    }
+
+    if (packet.data.query.substr(0, 8) == 'TRUNCATE')
+    {
+      var q = packet.data.query;
+
+      q = q.substr(9); // remove TRUNCATE 
+
+      if (q.substr(0, 5) == 'TABLE')
+      {
+        q = q.substr(6); // remove TABLE
+      }
+
+      q = q.replace(/;/g, ''); // remove special chars
+      q = q.replace(/`/g, '');
+      q = q.trim();
+
+      this.emit('truncate', packet.data.schema, q);
+      this.emit('change', packet.data.schema, q, 'truncate');
+      this._vanilla(packet.data.schema, q, 'truncate');
+    }
+  }
+
+  _vanilla(schema, table, event)
+  {
+    if (this._hasVanillaEvents)
+    {
+      this.emit(schema, table, event);
+      this.emit(table, event);
+      this.emit(schema + '.' + table, event);
+      this.emit(table + '.' + event);
+      this.emit(schema + '.' + table + '.' + event);
+    }
+  }
+
+
+  /**
+   * @param Function [cb]
+   */
+  start(cb)
+  {
+    this._mbe.start(cb);
+  }
+
+
+  /**
+   * @param Function [cb]
+   */
+  stop(cb)
+  {
+    this._mbe.stop(cb);
+  }
+
+
+  /**
+   * @param Function [cb]
+   */
+  restart(cb)
+  {
+    this._mbe.restart(cb);
+  }
+
+
+}
+
+module.exports = MyEmitter;
